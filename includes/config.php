ca9bea0088152c45827d0757aa2ba77795eaa9ae
Elif Nur Beycan <162906315+elifnurbeycan@users.noreply.github.com>
2025-06-15 21:42:51 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,15 +2,14 @@
 /**
  * config.php
  * Veritabanı bağlantı bilgilerini ve diğer genel uygulama ayarlarını içerir.
- * Bu dosyadaki hassas bilgileri GitHub'a yüklerken sansürlemeyi unutmayın!
  */
 
 // Veritabanı bağlantı sabitleri
 define('DB_SERVER', 'localhost'); // Veritabanı sunucusu adresi (genellikle localhost)
-define('DB_USERNAME', 'root');     // Veritabanı kullanıcı adı
-define('DB_PASSWORD', '');         // Veritabanı parolası (XAMPP/WAMP'ta genellikle boş)
-define('DB_NAME', 'family_budget_db'); // Veritabanı adı (daha önce oluşturduğumuz)
+define('DB_USERNAME', '*****');     // Veritabanı kullanıcı adı
+define('DB_PASSWORD', '*****');         // Veritabanı parolası (XAMPP/WAMP'ta genellikle boş)
+define('DB_NAME', '*****'); // Veritabanı adı (daha önce oluşturduğumuz)
 
 // Diğer uygulama ayarları (ileride eklenebilir)
-// define('BASE_URL', 'http://localhost/aile_butcesi_projesi/');
+define('BASE_URL', '/~st24360859210/');
 ?>
